dc5a51668ecc8851a95f8d852a0f63a95547a012
Ennio Marke <mystical.ennio@gmail.com>
2024-09-08 18:05:54 +0200

    no more supabase

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import webServer from './managers/web-server.js';
-import sbManager from './managers/supabase.js';
 import pbManager from './managers/pocketbase.js';
 import config from './config.js';
 import log from './managers/logger.js';
@@ -11,7 +10,6 @@ global.config = config;
 log('info', 'Starting server...');
 
 const web_server = new webServer();
-const sb_manager = new sbManager();
 const pb_manager = new pbManager();
 
 web_server.start(pb_manager.pb);
