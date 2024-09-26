9ebab62c82f88a93948c935a5cc94d63bb2d0ebb
Ennio Marke <mystical.ennio@gmail.com>
2024-09-26 14:30:38 +0200

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
     logger: {
-        timestamps: true,
+        timestamps: false,
     },
     auth: {
         enabled: false, //if true, user must be authenticated to access
@@ -10,5 +10,5 @@ export default {
         freeChange: true, //if true, user can change priority on their own
         reverse: true, //if true, the highest priority is the lowest number
     },
-    sessionTimeout: 6, //in hours
+    sessionTimeout: 3, //in hours
 };
